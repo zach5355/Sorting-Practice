763ce234ca7da8d93a8441011f13481567b815b7
zach5355 <snowboarder5355@gmail.com>
2020-12-30 19:31:07 -0600

    Initial Commit

---
+++ SortingPractice/SortingPractice/QuickSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingPractice
+{
+    public class QuickSort
+    {
+        public void Sort(int[]arr)
+        {
+            QuickSorter(arr,0,arr.Length-1);
+        }
+
+        private void QuickSorter(int[] arr,int start,int end)
+        {
+            if (start >= end)
+                return;
+            int index = Partition(arr, start, end);
+            QuickSorter(arr, start, index - 1);
+            QuickSorter(arr, index + 1, end);
+        }
+
+        private int Partition(int[] arr, int start, int end)
+        {
+            int pivot = 0;
+            for(int i =0; i < end; i++)
+            {
+                if(arr[i] < arr[end])
+                {
+                    Swap(arr, pivot++, i);
+                }
+            }
+            Swap(arr,pivot, end);
+            return pivot;
+        }
+        private void Swap(int[] arr, int first, int second)
+        {
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+    }
+}
---
+++ SortingPractice/SortingPracticeTests/QuickSortTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingPractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SortingPractice;
+
+namespace SortingPractice.Tests
+{
+    [TestClass()]
+    public class QuickSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            int[] arr = new int[] {1,4,9,29,30,2,84,32,10 };
+            int[] arrSorted = new int[] { 1,2, 4, 9, 10 ,29, 30, 32, 84};
+            QuickSort sort = new QuickSort();
+            sort.Sort(arr);
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] != arrSorted[i])
+                {
+                    Assert.Fail();
+                }
+            }
+
+        }
+    }
+}
